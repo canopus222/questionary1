5aaf03b1190323985b617a4ef12f41bc902a3f45
star_ <star_berry2s@yahoo.co.jp>
2025-01-29 14:50:34 +0900

    Set up database schema and related files

--- lib/questionary1/DB/Schema.pm
+++ lib/questionary1/DB/Schema.pm
@@ -3,14 +3,16 @@ use strict;
 use warnings;
 use utf8;
 
+# Tengのスキーマ定義使うためモジュールインポート。DBテーブルとPerlオブジェクトを簡単に対応させるための構文が提供
 use Teng::Schema::Declare;
 
 base_row_class 'questionary1::DB::Row';
 
+# テーブル member のカラムを定義
 table {
     name 'member';
     pk 'id';
-    columns qw(id name);
+    columns qw(id name age occupation drink remarks);
 };
 
 1;
--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,4 +1,8 @@
 CREATE TABLE IF NOT EXISTS member (
-    id           INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name         VARCHAR(255)
-);
+    id          INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    age         INTEGER NOT NULL,
+    occupation  ENUM('会社員', '自営業', '学生', '主婦（夫）', 'その他') NOT NULL,
+    drink       ENUM('ビール', 'ワイン', 'シャンパン', '日本酒', '紹興酒', '焼酎', 'ウイスキー') NOT NULL,
+    remarks     TEXT
+)
